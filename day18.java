99945378d2dccddac102099054abee1c38089352
Luis Moreno <luis.moreno@sap.com>
2022-07-25 21:20:45 -0500

    Add day 18, day 19,day 20

---
+++ day18.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    // Write your code here.
+    Stack<Character> s;
+    Queue<Character> q;
+    
+    public Solution(){
+        s= new Stack<>();
+        q= new LinkedList<>();
+    }
+       
+    public void pushCharacter(Character ch){
+        s.push(ch);
+    }
+    public void enqueueCharacter(Character ch){
+        q.add(ch);
+    }
+    public char popCharacter(){
+        return s.pop();
+    }
+    public char dequeueCharacter(){
+        return q.poll();
+    } 
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        scan.close();
+
+        // Convert input String to an array of characters:
+        char[] s = input.toCharArray();
+
+        // Create a Solution object:
+        Solution p = new Solution();
+
+        // Enqueue/Push all chars to their respective data structures:
+        for (char c : s) {
+            p.pushCharacter(c);
+            p.enqueueCharacter(c);
+        }
+
+        // Pop/Dequeue the chars at the head of both data structures and compare them:
+        boolean isPalindrome = true;
+        for (int i = 0; i < s.length/2; i++) {
+            if (p.popCharacter() != p.dequeueCharacter()) {
+                isPalindrome = false;                
+                break;
+            }
+        }
+
+        //Finally, print whether string s is palindrome or not.
+        System.out.println( "The word, " + input + ", is " 
+                           + ( (!isPalindrome) ? "not a palindrome." : "a palindrome." ) );
+    }
+}
